b18cdf832538323f86bb2e34a0f82c895a13fc10
Carlos Eduardo <65124135+KingTeox@users.noreply.github.com>
2023-01-10 16:32:14 -0300

    fix: pasta debug

--- debug/models/todo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const schemaTodo = new mongoose_1.Schema({
-    _id: { type: String, required: true },
-    date: { type: Date, default: new Date() },
-    message: { type: String, required: true }
-});
-exports.default = (0, mongoose_1.model)("todo-t", schemaTodo, "todo-collection");
